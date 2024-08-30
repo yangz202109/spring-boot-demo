a8086b2279c7bff52960ad49df24b205f0a017d5
yangz <3260519078@qq.com>
2024-08-30 18:40:46 +0800

    feat:去除模板enjoy整合

---
+++ demo-pay/src/main/java/com/study/Main.java
@@ -0,0 +1,11 @@
+package com.study;
+
+/**
+ * @author yangz
+ * @date 2024/8/30 - 18:37
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
