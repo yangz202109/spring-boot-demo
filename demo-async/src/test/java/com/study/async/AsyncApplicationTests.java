4fc954728a030935acc14fac77b6a1644dbc5e77
yangz202109 <129831835+yangz202109@users.noreply.github.com>
2024-09-12 14:57:32 +0800

    Update AsyncApplicationTests.java

--- demo-async/src/test/java/com/study/async/AsyncApplicationTests.java
+++ demo-async/src/test/java/com/study/async/AsyncApplicationTests.java
@@ -5,27 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class AsyncApplicationTests {
-    @Resouce
-    private TaskService taskService;
-
     @Test
     public void contextLoads() {
-    }
-
-    @Test
-    public void test() throws Exception {
-        long start = System.currentTimeMillis();
-     
-        Future<String> task = taskService.syncTaskGetResult();
-        while(true) {
-            if(task.isDone() ) {
-                // 任务都调用完成，退出循环等待
-                break;
-            }
-            Thread.sleep(1000);
-        }
-        long end = System.currentTimeMillis();
-        System.out.println("任务全部完成，总耗时：" + (end - start) + "毫秒");
-    }
+    } 
 }
 
