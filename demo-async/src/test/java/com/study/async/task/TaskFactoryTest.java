8f633488331be48254695cc5225f824e70f92a95
yangz202109 <129831835+yangz202109@users.noreply.github.com>
2024-09-12 14:58:53 +0800

    Update TaskFactoryTest.java

--- demo-async/src/test/java/com/study/async/task/TaskFactoryTest.java
+++ demo-async/src/test/java/com/study/async/task/TaskFactoryTest.java
@@ -24,4 +24,20 @@ public class TaskFactoryTest extends AsyncApplicationTests {
         taskService.syncTask();
         log.info("测试方法执行完毕");
     }
+
+    @Test
+    public void test() throws Exception {
+        long start = System.currentTimeMillis();
+     
+        Future<String> task = taskService.syncTaskGetResult();
+        while(true) {
+            if(task.isDone() ) {
+                // 任务都调用完成，退出循环等待
+                break;
+            }
+            Thread.sleep(1000);
+        }
+        long end = System.currentTimeMillis();
+        log.info("任务全部完成，总耗时：{} "毫秒"" , (end - start));
+    }
 }
